cc257d08acb83639467824a788937063f23c6ced
박수민 <141231525+ssumai-kr@users.noreply.github.com>
2024-02-26 22:43:11 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 3316 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+template <class T>
+class MinHeap {
+private:
+    T* heap;          // 힙 배열
+    int capacity;     // 힙의 용량
+    int heapSize;     // 힙에 저장된 요소의 개수
+
+public:
+    MinHeap(int initialCapacity = 10);
+    ~MinHeap();
+
+    void Push(const T& e);
+    void Pop();
+    const T& Top() const;
+    bool IsEmpty() const;
+};
+
+template <class T>
+MinHeap<T>::MinHeap(int initialCapacity) : capacity(initialCapacity), heapSize(0) {
+    heap = new T[capacity + 1]; // 인덱스 1부터 사용하기 위해 크기를 capacity + 1로 설정
+}
+
+template <class T>
+MinHeap<T>::~MinHeap() {
+    delete[] heap;
+}
+
+template <class T>
+void MinHeap<T>::Push(const T& e) {
+    if (heapSize == capacity) {
+        // 용량이 다 찼을 경우, 더 큰 크기의 배열로 확장
+        T* newHeap = new T[2 * capacity + 1];
+        for (int i = 1; i <= heapSize; ++i) {
+            newHeap[i] = heap[i];
+        }
+        delete[] heap;
+        heap = newHeap;
+        capacity = 2 * capacity;
+    }
+
+    int currentNode = ++heapSize;
+    while (currentNode != 1 && heap[currentNode / 2] > e) {
+        heap[currentNode] = heap[currentNode / 2];
+        currentNode /= 2;
+    }
+    heap[currentNode] = e;
+}
+
+template <class T>
+void MinHeap<T>::Pop() {
+    if (IsEmpty()) throw "Heap is empty. Cannot delete.";
+
+    heap[1].~T();
+    T lastE = heap[heapSize--];
+
+    int currentNode = 1;
+    int child = 2;
+    while (child <= heapSize) {
+        if (child < heapSize && heap[child] > heap[child + 1]) {
+            child++;
+        }
+        if (lastE <= heap[child]) {
+            break;
+        }
+        heap[currentNode] = heap[child];
+        currentNode = child;
+        child *= 2;
+    }
+    heap[currentNode] = lastE;
+}
+
+template <class T>
+const T& MinHeap<T>::Top() const {
+    if (IsEmpty()) throw "Heap is empty.";
+    return heap[1];
+}
+
+template <class T>
+bool MinHeap<T>::IsEmpty() const {
+    return heapSize == 0;
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+
+    vector<int> results;
+    MinHeap<int> Minh(N);
+
+    for (int i = 0; i < N; i++) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            if (Minh.IsEmpty()) results.push_back(0);
+
+            else {
+                results.push_back(Minh.Top());
+                Minh.Pop();
+            }
+        }
+        else Minh.Push(n);
+    }
+
+
+    for (int i = 0; i < results.size(); i++) {
+        cout << results[i] << "\n";
+    }
+}
