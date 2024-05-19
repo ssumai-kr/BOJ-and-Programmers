c038212d3dff3c463e91ba3b44adce9ef98865d5
박수민 <141231525+ssumai-kr@users.noreply.github.com>
2024-05-19 16:49:53 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 0.95 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    long long answer = 0;
+    int d=0, p=0, cnt=0;
+    
+    for(int i=n-1; i>=0; i--){
+        d-=deliveries[i];
+        p-=pickups[i];
+        
+        while(1){
+            if(d>=0 && p>=0)
+                break;
+            d+=cap;
+            p+=cap;
+            cnt++;
+        }
+        answer+=(i+1)*2*cnt;
+        cnt=0;
+    }
+    return answer;
+}
