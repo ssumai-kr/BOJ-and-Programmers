7d6eaf5d0557186908a36b61ae3c3b39077e673f
박수민 <141231525+ssumai-kr@users.noreply.github.com>
2025-07-13 01:52:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 260 ms, Memory: 6400 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 1001;
+vector<int> graph[MAX];
+bool visited[MAX];
+int n, m;
+
+void dfs(int node) {
+    visited[node] = true;
+    for (int next : graph[node]) {
+        if (!visited[next]) {
+            dfs(next);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            count++;
+        }
+    }
+
+    cout << count << "\n";
+    return 0;
+}
